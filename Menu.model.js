cf0bd1f9d8130af494f71438859cdf1d6d25a0a2
Gildardo Alvarado <gildardoalvarado2015519@gmail.com>
2020-08-18 21:41:50 -0600

    Ideas para la app

--- Menu.model.js
+++ Menu.model.js
@@ -3,8 +3,22 @@ const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
 const menuSchema = schema({
-    breakfast: [],
-    lunch:[],
-    price:[],
-    refaction:[]
+    breakfast:{ 
+        time: Date,
+        food: []
+    },
+    lunch:{ 
+        time: Date,
+        food: []
+    },
+    price:{ 
+        time: Date,
+        food: []
+    },
+    refaction:{ 
+        time: Date,
+        food: []
+    }
 })
+
+module.exports = mongoose.model('menu',menuSchema);
